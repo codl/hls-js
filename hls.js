15cc0aebdb0070762d21a5b9f41b4e174e6a4616
codl (Corentin Delcourt) <codl@codl.fr>
2015-07-13 21:00:09 +0200

    tests pass for variants

--- hls.js
+++ hls.js
@@ -6,6 +6,8 @@ function hls(input){
     out.segments = [];
     out.segments.low = 0;
 
+    out.playlists = [];
+
     for(var i = 0; i < lines.length; i++){
         var line = lines[i].trim();
 
@@ -16,6 +18,27 @@ function hls(input){
         else if(line.startsWith("#EXT-X-TARGETDURATION:")){
             out.type = "media";
             out.target_duration = line.substr("#EXT-X-TARGETDURATION:".length) - 0
+        }
+
+        else if(line.startsWith("#EXT-X-STREAM-INF:")){
+            out.type = "variant";
+            var playlist = {};
+            var args = line.substr("#EXT-X-STREAM-INF:".length).split(",");
+            for(var j = 0; j < args.length; j++){
+                var key = args[j].split("=")[0];
+                var value = args[j].split("=")[1];
+                if(key == "BANDWIDTH"){
+                    playlist.bandwidth = value - 0;
+                }
+                else if(key == "CODECS"){
+                    playlist.codecs = value.substr(1, value.length-2);
+                }
+                else if(key == "PROGRAM-ID"){
+                    playlist.program = value - 0;
+                }
+            }
+
+            out.playlists.push(playlist);
         }
 
         else if(line.startsWith("#EXT-X-MEDIA-SEQUENCE:")){
@@ -33,10 +56,15 @@ function hls(input){
         }
 
         else if(!line.startsWith("#") && line != ""){
-            out.segments[sequence].url = line;
-            out.segments.high = sequence;
+            if(out.type == "media"){
+                out.segments[sequence].url = line;
+                out.segments.high = sequence;
 
-            sequence += 1;
+                sequence += 1;
+            }
+            else {
+                out.playlists[out.playlists.length - 1].url = line;
+            }
         }
     }
 
